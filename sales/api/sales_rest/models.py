c21a1a3bbe51b89fbd662be46a81ad3ae9c59434
Paola Alcala <paolavalcala@gmail.com>
2023-09-05 18:23:20 -0700

    models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=100)
+    sold = models.BooleanField(default=False)
+
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Sale(models.Model):
+    price = models.CharField(max_length=100)
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sale",
+        on_delete=models.CASCADE,
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="sale",
+        on_delete=models.CASCADE,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sale",
+        on_delete=models.CASCADE,
+    )
+
+    def get_api_url(self):
+        return reverse("api_show_sale", kwargs={"pk": self.pk})
