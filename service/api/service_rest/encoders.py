93b21475fce48a8af057588db1659ad8dbb24ecd
DSnediker <dsnediker7@gmail.com>
2023-09-06 11:00:03 -0700

    created technician backend

---
+++ service/api/service_rest/encoders.py
@@ -0,0 +1,11 @@
+from common.json import ModelEncoder
+from .models import Technician
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
